4f589080aa032252a60a465605feeb8a57b6e306
the-kumar-amit07 <amitkumarmarndi37@gmail.com>
2024-06-23 23:08:54 +0530

    navbar added

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {authSlice} from "../store/authSlice.js"
 
 const store = configureStore({
     reducer: {
-        auth : authSlice,
+      
     }
 });
 
