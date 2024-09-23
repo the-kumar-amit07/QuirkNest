2843a34a14b1b6b4b2f076f4e3caca661ab173bd
the-kumar-amit07 <amitkumarmarndi37@gmail.com>
2024-09-23 22:11:23 +0530

    tinymce fixed

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -3,7 +3,8 @@ const conf ={
     appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinymceApiKey:import.meta.env.VITE_TINYMCE_API_KEY
 }
 console.log("Appwrite URL:", conf.appwriteUrl);  // Debugging check
 console.log("Project ID:", conf.appwriteProjectId);  // Debugging check
